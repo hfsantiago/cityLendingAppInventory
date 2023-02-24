b2869bda81942db7b724eb7833e482f1dbe253a3
Hugo <santiquiu@gmail.com>
2023-02-24 08:00:26 -0500

    js_functionality for FontEnd

--- app/javascript/controllers/delivery_collect_record_controller.js
+++ app/javascript/controllers/delivery_collect_record_controller.js
@@ -10,8 +10,16 @@ export default class extends Controller {
     console.log('Example');
     if (document.getElementById("delivery_collect_record_is_delivery").checked) {
       console.log('Checked');
+      document.getElementById("delivery_collect_record_nombre_recibe").removeAttribute("disabled");
+      document.getElementById("delivery_collect_record_nombre_devuelve").setAttribute("disabled", "");
+      document.getElementById("delivery_collect_record_devolucion_observaciones").setAttribute("disabled", "");
+      document.getElementById("delivery_collect_record_devolucion_estado_equipo").setAttribute("disabled", "");
     }else {
       console.log('Unchecked');
+      document.getElementById("delivery_collect_record_nombre_recibe").setAttribute("disabled", "");
+      document.getElementById("delivery_collect_record_nombre_devuelve").removeAttribute("disabled");
+      document.getElementById("delivery_collect_record_devolucion_observaciones").removeAttribute("disabled");
+      document.getElementById("delivery_collect_record_devolucion_estado_equipo").removeAttribute("disabled");
     }
   }
 }
