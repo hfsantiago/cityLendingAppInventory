4e12561ed8995aa53ff454e74341e7961dc36d07
Hugo <santiquiu@gmail.com>
2023-05-17 07:58:27 -0500

    added table for handling assets in D_C_R

--- app/models/delivery_collect_record.rb
+++ app/models/delivery_collect_record.rb
@@ -1,3 +1,6 @@
 class DeliveryCollectRecord < ApplicationRecord
   belongs_to :employee
+  has_many :delivery_collect_record_assets
+
+  accepts_nested_attributes_for :delivery_collect_record_assets
 end
