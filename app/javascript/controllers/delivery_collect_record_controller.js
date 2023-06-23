0ce14671f61577279e187da2316e24a69023278e
Hugo <santiquiu@gmail.com>
2023-06-23 07:56:16 -0500

    getting value from selected asset

--- app/javascript/controllers/delivery_collect_record_controller.js
+++ app/javascript/controllers/delivery_collect_record_controller.js
@@ -24,6 +24,7 @@ export default class extends Controller {
   }
 
   adding_assets() {
-    console.log('adding assets');
+    console.log('adding assets'); 
+    console.log(document.getElementById("asset"));
   }
 }
